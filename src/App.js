f7ae321376f27d65f643077d23633fac1f65879a
Conco <conco@MacBook-Pro-de-Usuario.local>
2024-11-22 11:27:51 -0300

    pokedex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PokemonList from './components/PokemonList';
+import PokemonDetail from './components/PokemonDetail';
+
+import pokemonData from './data/Pokedex.json';
+
+const formattedPokemonData = pokemonData.map(pokemon => ({
+  ndex: `#${pokemon.id.toString().padStart(4, '0')}`,
+  name: pokemon.name.english,
+  types: pokemon.type,
+  image: pokemon.image.sprite
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<PokemonList pokemonData={formattedPokemonData} />} />
+          <Route path="/pokemon/:id" element={<PokemonDetail />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
